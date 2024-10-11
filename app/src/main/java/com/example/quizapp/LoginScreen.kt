10991ebe7db9e65719d074e1432b7ec07c187fec
PriyanshuYadav08 <priyanshu81104@gmail.com>
2024-10-11 14:48:17 +0530

    Signup and Login function integrated and tested with firebase

--- app/src/main/java/com/example/quizapp/LoginScreen.kt
+++ app/src/main/java/com/example/quizapp/LoginScreen.kt
@@ -1,20 +1,58 @@
 package com.example.quizapp
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginScreen : AppCompatActivity() {
+    private lateinit var emailEditText: EditText
+    private lateinit var passwordEditText: EditText
+
+    lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_login_screen)
+
+        emailEditText = findViewById(R.id.email_enter_login)
+        passwordEditText = findViewById(R.id.password_enter_login)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val loginButton = findViewById<Button>(R.id.login_button_login)
+        loginButton.setOnClickListener {
+            login()
+        }
+    }
+
+    private fun login() {
+        val email = emailEditText.text.toString()
+        val password = passwordEditText.text.toString()
+
+        if (email.isBlank() || password.isBlank()){
+            Toast.makeText(this, "Email and Password can not be blank", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this) {
+            if (it.isSuccessful) {
+                Toast.makeText(this, "Login successful", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "User does not exist", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
--- app/src/main/java/com/example/quizapp/SignUpScreen.kt
+++ app/src/main/java/com/example/quizapp/SignUpScreen.kt
@@ -1,20 +1,65 @@
 package com.example.quizapp
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
 
 class SignUpScreen : AppCompatActivity() {
+    private lateinit var emailEditText: EditText
+    private lateinit var passwordEditText: EditText
+    private lateinit var confirmPasswordEditText: EditText
+
+    lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_sign_up_screen)
+
+        emailEditText = findViewById(R.id.email_enter)
+        passwordEditText = findViewById(R.id.password)
+        confirmPasswordEditText = findViewById(R.id.confirm_password)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val signUpButton = findViewById<Button>(R.id.sign_up)
+        signUpButton.setOnClickListener {
+            signupuser()
+        }
+    }
+
+    private fun signupuser() {
+        val email = emailEditText.text.toString()
+        val password = passwordEditText.text.toString()
+        val confirmPassword = confirmPasswordEditText.text.toString()
+
+        if (email.isBlank() || password.isBlank() || confirmPassword.isBlank()) {
+            Toast.makeText(this, "Email and password can not be blank", Toast.LENGTH_SHORT)
+                .show()
+            return
+        }
+        if (password != confirmPassword) {
+            Toast.makeText(this, "Password and Confirm password do not match", Toast.LENGTH_SHORT)
+                .show()
+            return
+        }
+
+        firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this) {
+            if (it.isSuccessful) {
+                Toast.makeText(this, "User created successfully", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "An error occurred", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
