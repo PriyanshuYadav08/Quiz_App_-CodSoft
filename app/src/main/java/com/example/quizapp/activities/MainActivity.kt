ea88682711b5b5e1afd9e7149cbc632ae5e59e54
PriyanshuYadav08 <priyanshu81104@gmail.com>
2024-10-28 14:17:38 +0530

    quiz list in main activity

--- app/src/main/java/com/example/quizapp/activities/MainActivity.kt
+++ app/src/main/java/com/example/quizapp/activities/MainActivity.kt
@@ -8,10 +8,16 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.quizapp.R
+import com.example.quizapp.adapters.quiz_adapter
+import com.example.quizapp.models.quiz
 
 class MainActivity : AppCompatActivity() {
     lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
+    lateinit var adapter: quiz_adapter
+    private var quiz_list = mutableListOf<quiz>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -22,10 +28,26 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        dummydata()
         setupviews()
     }
+
+    private fun dummydata() {
+        quiz_list.add(quiz("10","10"))
+        quiz_list.add(quiz("11","11"))
+        quiz_list.add(quiz("12","12"))
+        quiz_list.add(quiz("13","13"))
+    }
+
     fun setupviews(){
         setupdrawer()
+        setuprecyclerview()
+    }
+
+    private fun setuprecyclerview() {
+        adapter = quiz_adapter(this,quiz_list)
+        findViewById<RecyclerView>(R.id.apprecycler).layoutManager = GridLayoutManager(this,2)
+        findViewById<RecyclerView>(R.id.apprecycler).adapter = adapter
     }
 
     fun setupdrawer(){
